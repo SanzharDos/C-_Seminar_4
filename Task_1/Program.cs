c8fc83d1bc31ef0b4919f1eebae46ae0f4105af9
Sanzhar <dos.0227@yandex.ru>
2022-12-03 13:27:34 +0600

    Task1 function updated

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -2,8 +2,15 @@
 // 7 -> 28
 // 4 -> 10
 
+int SumNumbers(int num)
+{
+    int sum = 0;
+    for (int i = 1; i <= num; i++) sum += i;
+    return sum;
+}
+
 Console.WriteLine("Введите целое число");
 int num = Convert.ToInt32(Console.ReadLine());
-int sum = 0;
-for (int i = 1; i <= num; i++) sum+=i;
-Console.WriteLine($"Сумма всех чисел от 1 до {num} равна = {sum}");
+// int sum = 0;
+// for (int i = 1; i <= num; i++) sum+=i; // sum = sum + i;
+Console.WriteLine($"Сумма всех чисел от 1 до {num} равна = {SumNumbers(num)}");
